6342f83380572531991d2d673abc7c4461630887
Minh Le <baminh.letran@gmail.com>
2025-04-01 14:18:56 +0700

    Add frontend/tailwind.config.js 1743491936018

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        instagram: {
+          pink: '#E1306C',
+          purple: '#833AB4',
+          blue: '#405DE6'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
